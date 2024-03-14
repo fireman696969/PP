a41f6b91ad53834ed703c1734d00a3b3d372f923
Student11 <s55109@nhs.co.il>
2024-03-14 10:37:31 +0200

    14.3

--- app/src/main/java/com/example/pollutionpals/UI/NewReportPage/NewReportPage.java
+++ app/src/main/java/com/example/pollutionpals/UI/NewReportPage/NewReportPage.java
@@ -1,23 +1,32 @@
 package com.example.pollutionpals.UI.NewReportPage;
 
+import static android.text.InputType.TYPE_NULL;
+
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.DatePicker;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import com.example.pollutionpals.R;
 
+import java.util.Calendar;
+
 public class NewReportPage extends AppCompatActivity implements View.OnClickListener {
     ImageView imgvCamera;
     NewReportModule newReportModule;
     static final int REQUEST_CODE = 22;
+    EditText edDate;
+    int y=0,m=0,d=0;
 
 
 
@@ -25,9 +34,15 @@ public class NewReportPage extends AppCompatActivity implements View.OnClickList
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_report_page);
+
         imgvCamera = findViewById(R.id.imgvCamera);
         imgvCamera.setOnClickListener(this);
+
         newReportModule = new NewReportModule(this);
+
+        edDate = findViewById(R.id.edDate);
+        edDate.setOnClickListener(this);
+        edDate.setRawInputType(TYPE_NULL);
 
     }
 
@@ -36,6 +51,40 @@ public class NewReportPage extends AppCompatActivity implements View.OnClickList
         if (imgvCamera == view) {
             Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             startActivityForResult(intent, REQUEST_CODE);
+        }
+        if(edDate == view){
+            Calendar c = Calendar.getInstance();
+            int year = c.get(Calendar.YEAR);
+            int month = c.get(Calendar.MONTH);
+            int day = c.get(Calendar.DAY_OF_MONTH);
+            c.set(year,month,day);
+
+            Calendar c2 = Calendar.getInstance();
+            int year2 = c2.get(Calendar.YEAR)-70;
+            int month2 = c2.get(Calendar.MONTH);
+            int day2 = c2.get(Calendar.DAY_OF_MONTH);
+            c2.set(year2,month2,day2);
+
+            DatePickerDialog dateDialog = new DatePickerDialog(this);
+            dateDialog.setCancelable(false);
+
+            dateDialog.getDatePicker().setMaxDate(c.getTimeInMillis());
+            dateDialog.getDatePicker().setMinDate(c2.getTimeInMillis());
+
+            if(y!=0){
+                dateDialog.updateDate(y,m,d);
+            }
+            dateDialog.show();
+
+            dateDialog.setOnDateSetListener(new DatePickerDialog.OnDateSetListener() {
+                @Override
+                public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                    edDate.setText(i + "/ " + (i1+1) + " / " + i2);
+                    y=i;
+                    m= i1;
+                    d= i2;
+                }
+            });
         }
 
     }
